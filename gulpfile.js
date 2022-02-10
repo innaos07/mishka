943e3ce2fc68f8f636b8b41f6b1959bc3eb37458
Inna Oseledko <samosudova07@gmail.com>
2022-02-10 17:01:14 +0300

    change link

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,14 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 
+
+
+
 gulp.task("sass", function () {
     return gulp.src('./sass/**/*.scss')
     .pipe(sourcemaps.init())
+   
+
     .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./css'));
@@ -17,3 +22,5 @@ gulp.task("sass", function () {
 gulp.task("watch", function () {
     gulp.watch('./sass/**/*.scss', gulp.series('sass'))
 });
+
+
